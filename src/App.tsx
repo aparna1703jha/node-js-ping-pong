2caac59626fda6f81705e94921a32b553a8a1654
Aparna Jha <aparna1703jha@gmail.com>
2023-01-05 16:19:13 +0530

    7th commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 import InputField from "./components/InputField";
 import TodoList from './components/TodoList';
-import {Todo} from "./modal"
+import { Todo } from "./modal"
 
-const App : React.FC = () => {
-  
+const App: React.FC = () => {
+
   const [todo, setTodo] = useState<string>("")
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
 
-    if(todo){
-    setTodos([...todos, {id:Date.now(), todo ,isDone:false}])
-    setTodo("");
-     }
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }])
+      setTodo("");
+    }
   };
 
   console.log(todos);
@@ -23,8 +23,8 @@ const App : React.FC = () => {
   return (
     <div className="App">
       <span className="heading ">Taskify</span>
-      <InputField todo = {todo} setTodo = {setTodo} handleAdd = {handleAdd}/>
-      <TodoList todos={todos} setTodos={setTodos}/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
       {/* {todos.map((t) => (
         <li>{t.todo}</li>
       ))} */}
